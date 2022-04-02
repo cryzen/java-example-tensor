049af1cd4a069323c27fd7daa9772b82b6eb8d89
Cryzene <rsmurygin90@gmail.com>
2022-04-02 18:22:17 +0300

    redone solution 3

--- src/Solution3/Test.java
+++ src/Solution3/Test.java
@@ -4,22 +4,26 @@ public class Test {
     public static void main(String[] args){
         Card card1 = new Card();
 
-        System.out.println(card1.toString());
+        Card card3 = new Card("square", "sixteen");
+
+        System.out.println(card1.getRank());
         System.out.println(card1.hashCode());
 
-        Card card2 = new Card("spades", "four");
+        Card card2 = new Card("spades", "three");
+        Card card4 = new Card("spades", "three");
 
-        System.out.println(card2.toString());
+        System.out.println(card2);
         System.out.println(card2.hashCode());
 
         System.out.println();
 
-        System.out.println(card1.equals(card2));
-        System.out.println(card1.checkRank(card2));
-        System.out.println(card2.compareTo(card1));
+        System.out.println(card3.checkDeckMembership());
+        System.out.println(card4.equals(card2));
+        System.out.println(card4.checkRank(card2));
+        System.out.println(card4.compareTo(card2));
 
         System.out.println();
 
-        System.out.println(Card.compareCards(card1, card2));
+        System.out.println(Card.compareCards(card4, card2));
     }
 }
